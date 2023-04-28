efecbb39ff209d66cc97a0cc17ad49890a449e38
zainreza19 <zainreza1998@gmail.com>
2023-04-29 04:28:18 +0500

    Update slot_machine.py

--- slot_machine.py
+++ slot_machine.py
@@ -3,35 +3,31 @@ import calculator
 import random,math 
 
 
-# def play():
+def play():
 
-#     symbols=['🍒','🍓','💸']
+    symbols=['🍒','🍓','💸']
 
-#     results=random.choices(symbols,k=3)
+    results=random.choices(symbols,k=3)
 
-#     print(results[0]+'|'+results[1]+'|'+results[2])
+    print(results[0]+'|'+results[1]+'|'+results[2])
 
-#     if (results[0]=='💸' and results[1]=='💸' and results[2]=='💸'):
-#         print('JACKPOTTTTTT!')
-
-
-
-# is_play_again='Y'
-
-# play()
-
-# while(is_play_again=='Y'):
-#     is_play_again=str(input('Do you want to play again? (Y/N) '))
-#     play()
-
-
-# print('Thank you for playing. Please come again.')
+    if (results[0]=='💸' and results[1]=='💸' and results[2]=='💸'):
+        print('JACKPOTTTTTT!')
 
 
 
+is_play_again='Y'
+
+play()
+
+while(is_play_again=='Y'):
+    is_play_again=str(input('Do you want to play again? (Y/N) '))
+    play()
 
 
-calculator.add(4,5)
+print('Thank you for playing. Please come again.')
+
+
 
 
 
