cbdaa97aff9942b1e1ba4c6af343218cb07b7820
zainreza19 <zainreza1998@gmail.com>
2023-04-29 04:24:24 +0500

    Add files via upload

---
+++ slot_machine.py
@@ -0,0 +1,40 @@
+
+import calculator
+import random,math 
+
+
+# def play():
+
+#     symbols=['🍒','🍓','💸']
+
+#     results=random.choices(symbols,k=3)
+
+#     print(results[0]+'|'+results[1]+'|'+results[2])
+
+#     if (results[0]=='💸' and results[1]=='💸' and results[2]=='💸'):
+#         print('JACKPOTTTTTT!')
+
+
+
+# is_play_again='Y'
+
+# play()
+
+# while(is_play_again=='Y'):
+#     is_play_again=str(input('Do you want to play again? (Y/N) '))
+#     play()
+
+
+# print('Thank you for playing. Please come again.')
+
+
+
+
+
+calculator.add(4,5)
+
+
+
+
+
+
